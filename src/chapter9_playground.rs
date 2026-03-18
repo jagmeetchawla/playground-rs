5811973bc919308dbc7d1fb7b1f9b32efc60d7f3
Jagmeet Chawla <jagmeet.chawla@gmail.com>
2026-03-17 20:50:09 -0400

    completed chapter9, closures, traits and generics

---
+++ src/chapter9_playground.rs
@@ -0,0 +1,70 @@
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::path::Path;
+//use std::fs::File;
+pub fn run() {
+    println!("Running chapter 9 playground");
+
+    let filename = "content/hello.txt";
+
+    if Path::new(filename).exists() {
+        println!("file exists: {}", filename);
+        let contents = std::fs::read_to_string(filename).unwrap_or_else(|e| panic!("Failed to read file: {}", e));
+        println!("{}", contents);
+    } else {
+        println!("file does not exist: {}", filename);
+        let mut f = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(filename).unwrap_or_else(|e| panic!("Failed to create file: {}", e));
+        f.write_all(b"Hello, world!\n").unwrap_or_else(|e| panic!("Failed to write to file: {}", e));
+    }
+
+    /*
+    let mut file = File::open(filename).unwrap_or_else(
+        |e| match e.kind() {
+            std::io::ErrorKind::NotFound => {
+                let mut f = File::create(filename).unwrap_or_else(|e| panic!("Failed to create file: {}", e));
+                f.write_all(b"Hello, world 2!\n").unwrap();
+                f.flush().unwrap();
+                f
+            },
+            _ => panic!("Unexpected error: {}", e),
+        }
+    );
+    let mut contents = String::new();
+    if let Ok(content_length) = file.read_to_string(&mut contents) {
+        println!("Read {} bytes", content_length);
+    }
+    println!("{}", contents)
+    */
+    /*
+    match file {
+        Ok(mut f) => {
+            let mut contents = String::new();
+            f.read_to_string(&mut contents).unwrap();
+            println!("{}", contents);
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            println!("{:?} - {:?}", e.raw_os_error().unwrap(), e.kind());
+            match e.kind() {
+                std::io::ErrorKind::NotFound => {
+                    println!("File not found: {}", filename);
+                    let f = File::create(filename);
+                    match f {
+                        Ok(mut f) => {
+                            f.write_all(b"Hello, world 2!").unwrap();
+                            println!("Created new file {}", filename);
+                        },
+                        Err(_e) => {
+                            panic!("Failed to create file");
+                        }
+                    }
+                },
+                _ => panic!("Unexpected error"),
+            }
+        },
+    }
+    */
+}
---
+++ src/closures_playground.rs
@@ -0,0 +1,37 @@
+pub fn run() {
+    println!("Running closures playground");
+
+    let add = |x| x + 1;
+    println!("{}", add(5)); // 6
+
+    let sum = |x, y| x + y;
+    println!("{}", sum(5, 6));
+
+    let sum_of_three = |x: i32, y: i32, z: i32| x + y + z;
+    println!("{}", sum_of_three(1, 2, 3));
+
+    let v = vec![1, 2, 3];
+    let result: Vec<_> = v
+        .iter()
+        .map(|x| x * 2)
+        .collect();
+    println!("{:?}", result); // [2, 4, 6]
+
+    let x = 10;
+    let f = |y| x + y; // borrows x
+    println!("{}", f(5));
+
+    let mut count = 0;
+    let mut inc = || {
+        count += 1;
+    };
+    inc();
+    inc();
+    println!("{}", count); // 2
+
+    let s = String::from("hello");
+    let f = move || {
+        println!("{}", s);
+    };
+    f();
+}
